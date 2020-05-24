1a3a663f6ea558909a0025ff405bb88d47787782
michael-small <small203@morris.umn.edu>
2020-05-24 15:13:41 -0500

    Make calendar iframes responsively sized widths
    We managed to make it responsive to month vs agenda view, but the styling had the calendars either go offscreen or use too little of the screen. Moving the iframes to `Content.js` and setting `iframe {width: 100%}` resolved the issue as far as I can tell.
    
    It just occurred to me as I write this that this may make it harder to directly change the calendar in the CMS, but we could make a workaround if need be.

--- src/templates/HomePage.js
+++ src/templates/HomePage.js
@@ -19,12 +19,7 @@ export const HomePageTemplate = ({ title, subtitle, featuredImage, body }) => (
     <section className="section">
       <div className="container">
         <Content source={body} />
-        <div className="smallDisplay">
-        <iframe src="https://calendar.google.com/calendar/b/4/embed?height=400&amp;wkst=1&amp;bgcolor=%23B39DDB&amp;ctz=America%2FChicago&amp;src=ZGV2dGVzdGVyMmsyMEBnbWFpbC5jb20&amp;color=%23039BE5&amp;showPrint=0&amp;showTabs=0&amp;showTz=0&amp;showCalendars=0&amp;mode=AGENDA&amp;title=Does%20title%20even%20matter%3F&amp;showTitle=0&amp;showDate=0&amp;showNav=1" style={{borderWidth: "0"}} width="340" height="400" frameborder="0" scrolling="no"></iframe>
-        </div>
-        <div className="largeDisplay">
-        <iframe src="https://calendar.google.com/calendar/b/4/embed?height=400&amp;wkst=1&amp;bgcolor=%23B39DDB&amp;ctz=America%2FChicago&amp;src=ZGV2dGVzdGVyMmsyMEBnbWFpbC5jb20&amp;color=%23039BE5&amp;showPrint=0&amp;showTabs=0&amp;showTz=0&amp;showCalendars=0&amp;mode=MONTH&amp;title=Does%20title%20even%20matter%3F&amp;showTitle=0&amp;showDate=0&amp;showNav=1" style={{borderWidth: "0"}} width="400" height="400" frameborder="0" scrolling="no"></iframe>
-        </div>
+        
       </div>
     </section>
   </main>
