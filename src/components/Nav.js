e669d29d21ac842728a9001bb6ca4427ad376bf2
Utkarsh Kumar <kumar375@morris.umn.edu>
2020-05-28 13:42:03 -0500

    Refactor the Default page to Online Resources

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -83,7 +83,7 @@ export class Navigation extends Component {
                 </div>
               </span>
             </div>
-            <NavLink to="/default/">Default</NavLink>
+            <NavLink to="/default/">Online Resources</NavLink>
             <NavLink to="/contact/">Contact</NavLink>
           </div>
           <button
